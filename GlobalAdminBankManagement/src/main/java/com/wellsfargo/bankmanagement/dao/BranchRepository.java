bdb906b906101b62ae6c3a5e6c94e389daa9dacf
Saiteja0909 <55915674+Saiteja0909@users.noreply.github.com>
2022-11-08 14:50:22 +0530

    Delete GlobalAdminBankManagement directory

--- GlobalAdminBankManagement/src/main/java/com/wellsfargo/bankmanagement/dao/BranchRepository.java
+++
@@ -1,8 +0,0 @@
-package com.wellsfargo.bankmanagement.dao;
-
-import com.wellsfargo.bankmanagement.model.Branch;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BranchRepository extends JpaRepository<Branch, String> {}
--- GlobalAdminBankManagement/src/main/java/com/wellsfargo/bankmanagement/records/AccountRecord.java
+++
@@ -1,9 +0,0 @@
-package com.wellsfargo.bankmanagement.records;
-
-public record AccountRecord(
-        String account_number,
-        Long customer_number,
-        String branch_id,
-        Long balance,
-        String account_type
-) {}
